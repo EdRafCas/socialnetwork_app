062db7ee4f05845fc80789f5ede04733f3eac160
EdRafCas <Eduardoo.rafael@gmail.com>
2022-03-29 22:33:56 -0300

    main layout for design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+  height: 100vh;
+  border:solid black 15px;
+  display:grid;
+  grid-template-columns: repeat(1, 3fr 2fr);
+  justify-content: flex-start;
+  
+  position: relative;
+  z-index: 100;
+  min-height:750px;
+`
+const Timeline = styled.div`
+  
+  height:100%;
+  display:flex;
+  flex-direction:column;
+  padding:1rem;
+  border:solid red 1px;
+  gap:1rem;
+`
+const Card =styled.div`
+  padding:1rem;
+  display:flex;
+  flex-direction:column;
+  border:solid blue 1px;
+  gap:1rem;
+
+`
+const UserNameContainer =styled.div`
+  width:100%;
+  padding-left:1rem;
+  padding-top:5px;
+  padding-bottom:5px;
+  border:solid red 1px;
+  display:flex;
+  flex-direction:row;
+  gap:1rem;
+`
+
+const NameContainer =styled.h1`
+  border:solid black 1px;
+
+`
+
+const AliasContainer = styled.p`
+  border:solid black 1px;
+
+`
+
+const MessageContent = styled.div`
+  padding:1rem;
+  height:200px;
+  border:solid black 1px;
+`
+
+const AccountManagement = styled.div`
+  height:300px;
+  display:flex;
+  flex-direction:column;
+  border:solid red 1px;
+
+`
+
+const App = () => {
+  return ( 
+    <Container>
+      <Timeline>
+        <Card>
+          <UserNameContainer>
+            <NameContainer>hi</NameContainer><AliasContainer>hello</AliasContainer>
+          </UserNameContainer>
+          <MessageContent>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </MessageContent>
+        </Card>    
+        <Card>
+          <UserNameContainer>
+            <NameContainer>hi</NameContainer><AliasContainer>hello</AliasContainer>
+          </UserNameContainer>
+          <MessageContent>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </MessageContent>
+        </Card>    
+        
+      </Timeline>
+      <AccountManagement>
+        <h1>hi</h1>
+      </AccountManagement>
+      
+      
+    </Container>
+   );
 }
-
+ 
 export default App;
+
